93abf559fd6cc247dac77c1ca36ec6190e93e85a
Graham Hewett <graham.hewett@nimble-elearning.com>
2021-02-01 20:20:20 +0000

    remove logs

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,19 @@
 // Query Selectors
-const cityForm = document.querySelector('form');
-const details = document.querySelector('.details');
-const card = document.querySelector('.card');
-const time = document.querySelector('img.time');
-const icon = document.querySelector('.icon img');
+const cityForm = document.querySelector("form");
+const details = document.querySelector(".details");
+const card = document.querySelector(".card");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
 
 // Update User Function
 
-const updateUI = data => {
-	const cityDets = data.cityDets;
-	const weather = data.weather;
+const updateUI = (data) => {
+  const cityDets = data.cityDets;
+  const weather = data.weather;
 
-	// Update Details Template String
+  // Update Details Template String
 
-	details.innerHTML = `
+  details.innerHTML = `
     <div class="text-muted text-uppercase text-center details">
         <h5 class="my-3">${cityDets.EnglishName}</h5>
         <div class="my-3">${weather.WeatherText}</div>
@@ -23,58 +23,56 @@ const updateUI = data => {
         </div>
     </div>`;
 
-	// Update Night and Day and Icon img
+  // Update Night and Day and Icon img
 
-	const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
-	icon.setAttribute('src', iconSrc);
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute("src", iconSrc);
 
-	let timeSrc = null;
+  let timeSrc = null;
 
-	if (weather.IsDayTime === true) {
-		timeSrc = 'img/day.svg';
-	} else {
-		timeSrc = 'img/night.svg';
-	}
+  if (weather.IsDayTime === true) {
+    timeSrc = "img/day.svg";
+  } else {
+    timeSrc = "img/night.svg";
+  }
 
-	time.setAttribute('src', timeSrc);
+  time.setAttribute("src", timeSrc);
 
-	console.log(data);
-
-	if (card.classList.contains('d-none')) {
-		card.classList.remove('d-none');
-	}
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
 };
 
 //Update City
 
-const updateCity = async city => {
-	const cityDets = await getCity(city);
-	const weather = await getWeather(cityDets.Key);
+const updateCity = async (city) => {
+  const cityDets = await getCity(city);
+  const weather = await getWeather(cityDets.Key);
 
-	return {
-		cityDets: cityDets,
-		weather: weather
-	};
+  return {
+    cityDets: cityDets,
+    weather: weather,
+  };
 };
 
-cityForm.addEventListener('submit', e => {
-	e.preventDefault();
+cityForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-	const city = cityForm.city.value.trim();
-	cityForm.reset();
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
 
-	// Update User Interface
-	updateCity(city)
-		.then(data => updateUI(data))
-		.catch(err => console.log(err));
+  // Update User Interface
+  updateCity(city)
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
 
-	// Set Local Storage
-	localStorage.setItem('city', city);
+  // Set Local Storage
+  localStorage.setItem("city", city);
 });
 
 // Run Update City and Update User Interface if Data Exisits on Local Storage
-if (localStorage.getItem('city')) {
-	updateCity(localStorage.getItem('city'))
-		.then(data => updateUI(data))
-		.catch(err => console.log(err));
+if (localStorage.getItem("city")) {
+  updateCity(localStorage.getItem("city"))
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
 }
